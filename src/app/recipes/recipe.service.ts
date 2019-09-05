b5cdf7e20311afc140ef54eea1b0d83eaf05e013
Marta Niemiec <niemiec.marta@hotmail.com>
2019-09-06 00:22:27 +0200

    Adding ingredients to recipes and passing them to the ShoppingList

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,22 +1,39 @@
 import { Recipe } from './recipe.model';
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [
     new Recipe(
-      'A test recipe',
-      'This is simply a test',
-      'https://cdn.pixabay.com/photo/2017/06/21/22/42/recipe-2428928_960_720.jpg'
+      'Schnitzel',
+      'A super-tasty Schnitzel!',
+      'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
+      [
+        new Ingredient('Meet', 1),
+        new Ingredient('French Fries', 20)
+      ]
     ),
     new Recipe(
-      'A test recipe2',
-      'This is simply a test2',
-      'https://cdn.pixabay.com/photo/2017/06/21/22/42/recipe-2428928_960_720.jpg'
+      'Big Fat Burger',
+      'What else you need to say?',
+      'https://www.seriouseats.com/images/2015/06/20150612-vegetarian-burger-recap-04.jpg',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 20)
+      ]
     )
   ];
 
+  constructor(private shoppingListService: ShoppingListService) {}
+
   getRecipes() {
     return this.recipes.slice(); // slice to get only a copy of the recipes array
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.shoppingListService.addIngredients(ingredients);
   }
 }
